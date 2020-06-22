20d871c660a5a47d77d2e260dcac3acf62de89ad
Nik Osvalds <nosvalds@gmail.com>
2020-06-22 11:16:04 +0100

    set up initial state with test data to build JSX

---
+++ src/data/initialState.js
@@ -0,0 +1,55 @@
+const initialState = {
+    playersASide: 5,
+    team_A: {
+        name: "Team A",
+        positions: [
+            {
+                id: 1,
+                name: "John"
+            },
+            {
+                id: 2,
+                name: "Jacob"
+            },
+            {
+                id: 3,
+                name: "JinkleHiemer"
+            },
+            {
+                id: 4,
+                name: "Schmidt"
+            },
+            {
+                id: 5,
+                name: "Billy"
+            },
+        ]
+    },
+    team_B: {
+        name: "Team B",
+        positions: [
+            {
+                id: 1,
+                name: "Tommy"
+            },
+            {
+                id: 2,
+                name: "Bobby"
+            },
+            {
+                id: 3,
+                name: "Josephine"
+            },
+            {
+                id: 4,
+                name: "Carl"
+            },
+            {
+                id: 5,
+                name: "David"
+            },
+        ]
+    }
+  };
+  
+  export default initialState;
