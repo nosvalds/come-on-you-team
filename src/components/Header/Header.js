58b9c090cbfc29aa0fb389d994c57bd8935b94d8
Nik Osvalds <nosvalds@gmail.com>
2020-06-22 11:36:48 +0100

    create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <header className="main-header">
+        <h1 className="main-header-text">Come On You _____ !</h1>
+    </header>
+)
+
+export default Header;
