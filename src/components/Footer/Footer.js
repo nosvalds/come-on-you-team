4ba209e51aa21c6f4accaf0d2edf8a5514e4328c
Nik Osvalds <nosvalds@gmail.com>
2020-06-23 09:45:18 +0100

    add github link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => (
     <footer className="main-footer">
         <p>&#169; 2020 Nik Osvalds</p>
+        <a className="icon-link" href="https://github.com/nosvalds/come-on-you-team">
+            <FontAwesomeIcon icon={ faGithubSquare } />
+        </a>
     </footer>
 )
 
