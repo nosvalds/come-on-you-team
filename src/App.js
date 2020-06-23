b38e7f17aa71664c100cf5fe0961670e2d7570e9
Nik Osvalds <nosvalds@gmail.com>
2020-06-23 08:39:24 +0100

    adding comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-// Components
+// Standard Components
 import Header from './components/Header/Header'
-import PlayerEntry from './components/PlayerEntry';
 import Pitch from './components/Pitch/Pitch';
 import TeamInfo from './components/TeamInfo/TeamInfo';
 import Footer from './components/Footer/Footer';
 
+// React-Redux (state/dispatch passed from index.js)
+import PlayerEntry from './components/PlayerEntry';
 
 function App() {
   return (
