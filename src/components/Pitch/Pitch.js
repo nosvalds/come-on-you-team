36f7b92e473150c62371bbdb35993528643b9282
Nik Osvalds <nosvalds@gmail.com>
2020-06-23 09:32:13 +0100

    make pitch responsive and hold aspect ratio

--- src/components/Pitch/Pitch.js
+++ src/components/Pitch/Pitch.js
@@ -5,13 +5,15 @@ import Offense from '../Offense';
 
 const Pitch = () => {
     return (
-        <section className="pitch-container">
-            <Goal team="team_A"/>
-            <Defense team="team_A" />
-            <Offense team="team_A" />
-            <Offense team="team_B" />
-            <Defense team="team_B" />
-            <Goal team="team_B"/>
+        <section className="pitch-container-background">
+            <div className="pitch-container-inside">
+                <Goal team="team_A"/>
+                <Defense team="team_A" />
+                <Offense team="team_A" />
+                <Offense team="team_B" />
+                <Defense team="team_B" />
+                <Goal team="team_B"/>
+            </div>
         </section>
     )
 }
