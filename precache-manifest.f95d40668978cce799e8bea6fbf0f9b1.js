134461207e3a53175fc412e20b92d1c058dae0e8
Nik Osvalds <nosvalds@gmail.com>
2020-06-23 09:54:00 +0100

    Updates

---
+++ precache-manifest.f95d40668978cce799e8bea6fbf0f9b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1773d9acf8617c2296badf016c911327",
+    "url": "/come-on-you-team/index.html"
+  },
+  {
+    "revision": "7bf99fd2e84b32c0ca2e",
+    "url": "/come-on-you-team/static/css/main.4c83470f.chunk.css"
+  },
+  {
+    "revision": "088270a617c98e375b87",
+    "url": "/come-on-you-team/static/js/2.39c28205.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/come-on-you-team/static/js/2.39c28205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf99fd2e84b32c0ca2e",
+    "url": "/come-on-you-team/static/js/main.1a1101bb.chunk.js"
+  },
+  {
+    "revision": "667ae7d17eaa881e5ee4",
+    "url": "/come-on-you-team/static/js/runtime-main.c5a4b573.js"
+  },
+  {
+    "revision": "9f55767adde222b4b8826ff80185b801",
+    "url": "/come-on-you-team/static/media/pitch.9f55767a.png"
+  }
+]);
