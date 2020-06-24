56947a1da864dbc2f75e5a2b8081c493742374f5
Nik Osvalds <nosvalds@gmail.com>
2020-06-24 21:34:59 +0100

    Updates

--- precache-manifest.a5be6db15107ee53c35f172a7e707359.js
+++ precache-manifest.a5be6db15107ee53c35f172a7e707359.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1773d9acf8617c2296badf016c911327",
+    "revision": "a2032adb901f0f65cf372af3cbe100f0",
     "url": "/come-on-you-team/index.html"
   },
   {
-    "revision": "7bf99fd2e84b32c0ca2e",
-    "url": "/come-on-you-team/static/css/main.4c83470f.chunk.css"
+    "revision": "40fdcfb59e65ddb51fd9",
+    "url": "/come-on-you-team/static/css/main.222e90ec.chunk.css"
   },
   {
-    "revision": "088270a617c98e375b87",
-    "url": "/come-on-you-team/static/js/2.39c28205.chunk.js"
+    "revision": "1fedabab9c08d20267b2",
+    "url": "/come-on-you-team/static/js/2.2a83be3d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/come-on-you-team/static/js/2.39c28205.chunk.js.LICENSE.txt"
+    "url": "/come-on-you-team/static/js/2.2a83be3d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7bf99fd2e84b32c0ca2e",
-    "url": "/come-on-you-team/static/js/main.1a1101bb.chunk.js"
+    "revision": "40fdcfb59e65ddb51fd9",
+    "url": "/come-on-you-team/static/js/main.4fd44503.chunk.js"
   },
   {
     "revision": "667ae7d17eaa881e5ee4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/come-on-you-team/precache-manifest.f95d40668978cce799e8bea6fbf0f9b1.js"
+  "/come-on-you-team/precache-manifest.a5be6db15107ee53c35f172a7e707359.js"
 );
 
 self.addEventListener('message', (event) => {
