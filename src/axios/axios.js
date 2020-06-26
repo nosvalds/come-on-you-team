92c9e4876ea2b6e80f6583ac6491995c5287f4b1
Nik Osvalds <nosvalds@gmail.com>
2020-06-26 16:28:20 +0100

    update axios with heroku prd url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: (process.env.NODE_ENV === 'production') ? 
-    "https://comeonyou.developme.space/api/" 
-    : "http://192.168.10.13/api/matches/",
+    "https://come-on-you-team.herokuapp.com/api/matches" // production url
+    : "http://192.168.10.13/api/matches/", // url from vagrant/homestead virtual machine for development
   headers: {
     Accept: "application/json",
   }
