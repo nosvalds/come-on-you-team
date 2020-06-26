c7b1b9a9795db50e0d250bbdde8ff194ce9ad839
Nik Osvalds <nosvalds@gmail.com>
2020-06-26 17:10:19 +0100

    update axios url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: (process.env.NODE_ENV === 'production') ? 
-    "https://come-on-you-team.herokuapp.com/api/matches" // production url
-    : "http://192.168.10.13/api/matches/", // url from vagrant/homestead virtual machine for development
+    "https://cors-anywhere.herokuapp.com/https://come-on-you-team.herokuapp.com/api/matches" // production url
+    : "https://cors-anywhere.herokuapp.com/https://come-on-you-team.herokuapp.com/api/matches", // url from vagrant/homestead virtual machine for development
   headers: {
     Accept: "application/json",
   }
