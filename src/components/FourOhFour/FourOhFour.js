05d72466816728e5f5ed7a9f4dabd5e1ac80b5e5
Nik Osvalds <nosvalds@gmail.com>
2020-06-26 09:21:12 +0100

    style 404

--- src/components/FourOhFour/FourOhFour.js
+++ src/components/FourOhFour/FourOhFour.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const FourOhFour = () => (
     <section className="four-oh-four">
-        <h4>
+        <h3>
             Page not found! Head <a href="/">home</a> to start over.
-        </h4>
+        </h3>
     </section>
 )
 export default FourOhFour;
