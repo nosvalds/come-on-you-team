ec0e1ddcb8772b44e8b40b07883373440015896f
Nik Osvalds <nosvalds@gmail.com>
2020-06-27 11:19:57 +0100

    fix axios setup for development

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: (process.env.NODE_ENV === 'production') ? 
     "https://cors-anywhere.herokuapp.com/https://come-on-you-team.herokuapp.com/api/matches" // production url
-    : "https://cors-anywhere.herokuapp.com/https://come-on-you-team.herokuapp.com/api/matches", // url from vagrant/homestead virtual machine for development
+    : "http://homestead.test/api/matches", // url from vagrant/homestead virtual machine for development
   headers: {
     Accept: "application/json",
   }
